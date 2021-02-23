451345c54ca9fe3f45fc0f29b86878aaf1ad5167
fernal73 <linus.fernandes@gmail.com>
2021-02-23 11:23:03 +0530

    Add examples

---
+++ runcpp.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#by Nacer Salah eddine
+
+
+#Script Used for compiling and running openmpi programs
+clear
+source_file_name="./pp_lab3.cpp"
+output_file_name="./prgm"
+no_of_process=5
+
+echo ---------Compiling Program------------
+
+mpic++ $source_file_name  -o $output_file_name -Wall
+if [ $? == 0 ];then #No Errors Detected
+	echo "COMPILE SUCCES"
+	echo ---------Executing Program------------
+    termux-elf-cleaner $output_file_name &> /dev/null 
+    mpirun --allow-run-as-root --hostfile hostfile -np $no_of_process $output_file_name
+	
+else
+	echo "COMPILE ERROR"
+fi
+
+
+#END OF SCRIPT
